8973f8fe0d4e43c033f2e16a2ea094c320549835
Your Name <miriti9595@gmail.com>
2022-08-18 02:10:00 -0700

    bitmanipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,11 +8,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int nbits;
-for (nbits = 0; n || m; n >>= 1, m >>= 1)
-if ((n & 1) != (m & 1))
-nbits++;
+int i, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+for (i = 63; i >= 0; i--)
+{
+current = exclusive >> i;
+if (current & 1)
+count++;
 }
-return (nbits);
+return (count);
 
 }
