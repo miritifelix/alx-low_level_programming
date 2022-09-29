1dda8a1b3a54d4c85c5306ebc042ea090022ea51
miritifelix <miriti9595@gmail.com>
2022-09-29 07:08:43 -0700

    Double linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * sum_dlistint - returns the sum of all the data (n)
  * of a doubly linked list
- * head: head of the list
+ * @head: head of the list
  * Return: sum of the data
  */
 int sum_dlistint(dlistint_t *head)
